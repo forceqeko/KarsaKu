5ec7ea138c484a45ef11c4bcb33d897890507660
forceqeko <ekoadytia19@gmail.com>
2026-07-20 12:28:44 +0800

    Fix build: generate gradle-wrapper.jar in CI, remove unused R import

--- app/src/main/java/com/karsaku/notification/NotificationHelper.kt
+++ app/src/main/java/com/karsaku/notification/NotificationHelper.kt
@@ -10,7 +10,6 @@ import android.media.AudioAttributes
 import android.media.RingtoneManager
 import android.os.Build
 import androidx.core.app.NotificationCompat
-import com.karsaku.R
 import com.karsaku.alarm.AlarmSchedulerImpl
 import com.karsaku.ui.alarm.AlarmActivity
 
